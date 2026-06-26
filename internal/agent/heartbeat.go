6463d60fb9932ac307faa5fbe919b622b51ba130
William Moreno Reyes <williamjmorenor@gmail.com>
2026-06-26 11:07:27 -0400

    chore: bump version to 0.0.1beta12

--- internal/agent/heartbeat.go
+++ internal/agent/heartbeat.go
@@ -20,7 +20,7 @@ import (
 	"github.com/admiral-project/admiral/admirald/pkg/admiral"
 )
 
-const FleetVersion = "0.0.1beta11"
+const FleetVersion = "0.0.1beta12"
 
 func (a *Agent) StartHeartbeatSender(ctx context.Context) {
 	time.Sleep(10 * time.Second)
