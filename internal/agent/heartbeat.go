874391ba353f57120a412416154322bb16652989
William Moreno Reyes <williamjmorenor@gmail.com>
2026-07-14 15:32:54 -0400

    chore(release): bump to 0.0.1beta16
    Signed-off-by: William Moreno Reyes <williamjmorenor@gmail.com>

--- internal/agent/heartbeat.go
+++ internal/agent/heartbeat.go
@@ -20,7 +20,7 @@ import (
 	"github.com/admiral-project/admiral/admirald/pkg/admiral"
 )
 
-const FleetVersion = "0.0.1beta15"
+const FleetVersion = "0.0.1beta16"
 
 func (a *Agent) StartHeartbeatSender(ctx context.Context) {
 	time.Sleep(10 * time.Second)
